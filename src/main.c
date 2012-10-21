1c1c4f67a25746d6a6bd32b83d2e972e1ee574e3
Jenner Hanni <jeh.wicker@gmail.com>
2012-10-21 14:32:39 -0700

    Testing if this compiles.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,28 @@
  * Licensed under the BSD three-clause license. See LICENSE.
  */
 
-#include "matrix.h"
-#include "kalman_filter/use_kf.h"
+#include "input.h"
+
+int main(void) {
+
+//  int filter_type = receive_input();
+  int filter_type = 0;
+
+  switch(filter_type) {
+    case 0: 
+      printf("Using linear kalman filter."\n);
+      break;
+    case 1:
+      printf("Using extended kalman filter."\n);
+      break;
+    case 2:
+      printf("Using unscented kalman filter."\n);
+      break;
+    else: 
+      printf("Failure."\n);
+      break;
+  }
+
+  return 0;
+}
 
