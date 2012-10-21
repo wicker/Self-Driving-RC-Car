2b01ac97cc0d28a0785947105cf6b99edd545417
Jenner Hanni <jeh.wicker@gmail.com>
2012-10-21 14:12:11 -0700

    Figuring out structure.

---
+++ src/main.c
@@ -0,0 +1,7 @@
+/* Copyright (c) 2012 Jenner Hanni <jeh.wicker@gmail.com>
+ * Licensed under the BSD three-clause license. See LICENSE.
+ */
+
+#include "matrix.h"
+#include "kalman_filter/use_kf.h"
+
