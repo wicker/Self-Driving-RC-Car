b8444ad1a14ffa76e458c87c91995d66963c0785
Jenner Hanni <jeh.wicker@gmail.com>
2012-10-22 16:56:52 -0700

    Removed matrix math functions to their own file.

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
  */
 
 #include "stdio.h"
+#include "math.h"
 
 #define ARGS 3
 
 int instructions();
-void transpose(int (*)[2]);
 
 int main(int argc, char *argv[]) {
 
@@ -42,34 +42,6 @@ int main(int argc, char *argv[]) {
   printf("a: %d %d\n   %d %d\n\n",a[0][0],a[0][1],a[1][0],a[1][1]);
 
   return 0;
-}
-
-void transpose(int a[0][2]) {
-  int row = 0;
-  int col = 0;
-  int new_row = 0;
-  int new_col = 0;
-
-  int temp[2][2];
-
-  for (row = 0; row < 2; row++)
-  {
-      for (col = 0; col < 2; col++)
-      {
-          temp[new_row][new_col] = a[row][col];
-          new_row++;
-      }
-      new_col++;
-      new_row = 0;
-  }
-
-  for (row = 0; row < 2; row++)
-  {
-      for (col = 0; col < 2; col++)
-      {
-          a[row][col] = temp[row][col];
-      }
-  }
 }
 
 int instructions() {
