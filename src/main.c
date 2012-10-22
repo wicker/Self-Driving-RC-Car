20953c070f1c44c6f13cd3afd8a618f31b63bb51
Jenner Hanni <jeh.wicker@gmail.com>
2012-10-22 16:53:24 -0700

    Transpose function works. Might have the hang of this multidimensional array thing.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #define ARGS 3
 
 int instructions();
+void transpose(int (*)[2]);
 
 int main(int argc, char *argv[]) {
 
@@ -32,7 +33,43 @@ int main(int argc, char *argv[]) {
       break;
   }
 
+  int a[2][2] = {1,2,3,4};
+
+  printf("a: %d %d\n   %d %d\n\n",a[0][0],a[0][1],a[1][0],a[1][1]);
+
+  transpose(a);
+
+  printf("a: %d %d\n   %d %d\n\n",a[0][0],a[0][1],a[1][0],a[1][1]);
+
   return 0;
+}
+
+void transpose(int a[0][2]) {
+  int row = 0;
+  int col = 0;
+  int new_row = 0;
+  int new_col = 0;
+
+  int temp[2][2];
+
+  for (row = 0; row < 2; row++)
+  {
+      for (col = 0; col < 2; col++)
+      {
+          temp[new_row][new_col] = a[row][col];
+          new_row++;
+      }
+      new_col++;
+      new_row = 0;
+  }
+
+  for (row = 0; row < 2; row++)
+  {
+      for (col = 0; col < 2; col++)
+      {
+          a[row][col] = temp[row][col];
+      }
+  }
 }
 
 int instructions() {
