344167be12e4e29fc06ddb4180c575cbce062a47
Jenner Hanni <jeh.wicker@gmail.com>
2014-03-27 18:33:10 -0700

    Pardon the excessive pushing. But aren't we used to that by now?

--- src/v2-stm32f4-discovery/blinkled/.dep/crt0.o.d
+++
@@ -1,8 +0,0 @@
-build/obj/crt0.o: \
- /home/wicker/tools/ChibiOS_2.6.3/os/ports/GCC/ARMCMx/crt0.c \
- /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stdint.h \
- /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/stdint.h
-
-/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stdint.h:
-
-/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/stdint.h:
