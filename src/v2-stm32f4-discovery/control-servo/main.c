c7dccd04e98ffbebadd097c556c3cf82ff257d44
Jenner Hanni <jeh.wicker@gmail.com>
2014-04-20 12:48:19 -0700

    Verified this code works with Arduino, VNH5019, and STM32F4 running onboard car.

--- src/v2-stm32f4-discovery/control-servo/main.c
+++ src/v2-stm32f4-discovery/control-servo/main.c
@@ -1,7 +1,7 @@
 // Servo control
 //
-// Servo on pin PB8 uses TIM4 channel 3 on alternate function 2
 // TIM4 supports PWM driving.
+// Servo on pin PB8 uses TIM4 channel 3 on alternate function 2
 // PWMD4 is a HAL-defined driver associated with TIM4.
  
 #include "ch.h"
@@ -25,7 +25,7 @@ static PWMConfig pwmcfg = {
  
 int main(void)
 {
-  static int dir = LEFT, step = 100, width = 500; /* starts at .7ms, ends at 2.5ms */
+  static int step = 100, width = 500; /* starts at .7ms, ends at 2.5ms */
  
   halInit();
   chSysInit();
@@ -38,11 +38,10 @@ int main(void)
   while (TRUE) {
     pwmEnableChannel(&PWMD4, 2, width);
     // if all the way to the left or right, go back the way you came
-    if (width == 700) dir = RIGHT;
-    else if (width == 2500) dir = LEFT;
+    if (width <= 700) step = 100;
+    else if (width >= 2500) step = -100;
     // now increment the width by step
-    if (dir == RIGHT) width += step;
-    else if (dir == LEFT) width -= step;
+    width += step;
     // brief delay
     chThdSleepMilliseconds(100);
   }
